5c83b8a9d3336ebb9b98298cffbf2f1b7e822ead
unknown <lucasamorim.lra@gmail.com>
2018-01-27 18:49:28 -0200

    arquivos do projeto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import {Routes} from '@angular/router'
+import {HomeComponent} from 'app/home/home.component'
+import {AboutComponent} from 'app/about/about.component'
+import {RestaurantsComponent} from 'app/restaurants/restaurants.component'
+import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from 'app/restaurant-detail/reviews/reviews.component';
+import { MenuComponent } from 'app/restaurant-detail/menu/menu.component';
+
+export const ROUTES: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent, 
+        children: [
+            {path: '', redirectTo: 'menu', pathMatch: 'full'},
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewsComponent}
+
+        ]},
+    {path: 'about', component: AboutComponent}
+
+]
